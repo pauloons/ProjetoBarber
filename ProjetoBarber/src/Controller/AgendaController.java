d01cac548b19701c3d981592bd85c1351f187223
Paulo Alexandre <pauloons@gmail.com>
2020-05-28 23:41:55 -0300

    Comit do netBeans

--- ProjetoBarber/src/Controller/AgendaController.java
+++ ProjetoBarber/src/Controller/AgendaController.java
@@ -21,20 +21,38 @@ public class AgendaController {
         this.View = View;
         this.helper = new AgendaHelper(View);
     }
+    
+    
  public void atualizaTabela(){
+ //buscar uma lista de dados
+ //exibir uma lista na view agenda
+ //
      AgendamentoDAO agendamentoDao  = new AgendamentoDAO();
      ArrayList<Agendamento> agendamentos = agendamentoDao.selectAll();
       helper.preencherTabela(agendamentos);
  }   
  public void atualizaCliente(){
-     ClienteDAO clientedao = new ClienteDAO();
-     ArrayList<Cliente> preencherClientes = clientedao.selectAll();
-     helper.preencherClientes(preencherClientes);
-}
- public void atualizaServico(){
-    ServicoDAO servicodao =  new ServicoDAO();
-     ArrayList<Servico> preencherServico = servicodao.selectAll();
- helper.preencherServico(preencherServico);    
- }
- 
+        ClienteDAO clienteDAO = new ClienteDAO();
+        ArrayList<Cliente> clientes = clienteDAO.selectAll();
+        
+        helper.preencherClientes(clientes);
+    }
+    public void atualizaServico(){
+        ServicoDAO servicoDao=  new ServicoDAO();
+        ArrayList<Servico> servicos = servicoDao.selectAll();
+        
+        helper.preencherServicos(servicos);
+    }
+    public void atualizaValor(){
+        Servico servico = helper.obterServico();
+        helper.setarValor(servico.getValor());
+    }
+    public void agendar(){
+        Agendamento agendamento = helper.obterModelo();
+        new AgendamentoDAO().insert(agendamento);
+        atualizaTabela();
+        helper.limparTela();
+    }
+
+    
 }
